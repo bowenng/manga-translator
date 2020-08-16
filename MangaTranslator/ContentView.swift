851aa0254e5b0afde358d7d19fea517cfc14256d
Bowen Wu <bowen-wu@hotmail.com>
2020-08-16 18:36:06 -0400

    Initial Commit

---
+++ MangaTranslator/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MangaTranslator
+//
+//  Created by BOWEN WU on 8/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MangaTranslator/MangaTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaTranslatorApp.swift
+//  MangaTranslator
+//
+//  Created by BOWEN WU on 8/16/20.
+//
+
+import SwiftUI
+
+@main
+struct MangaTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
