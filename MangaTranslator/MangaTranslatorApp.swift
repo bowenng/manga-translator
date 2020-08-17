73e5d19d244645ee304d14da7f06b5f9774dd507
Bowen Wu <bowen-wu@hotmail.com>
2020-08-16 22:47:33 -0400

    pod project

--- MangaTranslator/MangaTranslatorApp.swift
+++ MangaTranslator/MangaTranslatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MangaTranslatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MangaPicker(translator: OfflineTranslator())
         }
     }
 }
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '14.0'
+
+use_frameworks!
+
+target 'MangaTranslator' do
+  pod 'SwiftyTesseract',    '~> 3.0'
+end
